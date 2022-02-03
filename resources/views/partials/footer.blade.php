cf66432b2623acb87517fc7dedb08ca55563334a
Giorgio <1999giorgio.monne@mail.com>
2022-02-03 09:30:26 -1000

    fine parte logica...parte css approssimativa perche non mi funzionano i bg img

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,18 +2,108 @@
 
     <div class="links">
 
+        {{-- <img src="{{asset('img/footer-bg-jpg')}}" alt=""> --}}
+
         <div class="container">
+            <nav>
+                <ul>
+                    <h2>DC COMICS</h2>
+                    <li>
+                        <a href="http://">dfsdsfdshf</a>
+                    </li>
+                    <li>
+                        <a href="http://">dsfhsdfhfh</a>
+                    </li>
+                    <li>
+                        <a href="http://">dfhdsfhdh</a>
+                    </li>
+                    <li>
+                        <a href="http://">dfhdhdsfh</a>
+                    </li>
+                    <li>
+                        <a href="http://">sdfhfsdhsdf</a>
+                    </li>
+                    <li>
+                        <a href="http://">sdfhsdfhf</a>
+                    </li>
+                    <li>
+                        <a href="http://">sdfhsdfhhsd</a>
+                    </li>
 
-            <div class="link">
+                    <h2>SHOP</h2>
+                    <li>
+                        <a href="http://">adfgadfgadf</a>
+                    </li>
+                    <li>
+                        <a href="http://">adfgadfgfd</a>
+                    </li>
+                    <li>
+                        <a href="http://">dfgsdgsdg</a>
+                    </li>
 
-            </div>
+                </ul>
 
-            <div class="dc">
-                <img src="" alt="">
-            </div>
 
+                <ul>
+                    <h2>DC</h2>
+                    <li>
+                        <a href="http://">dfsdsfdshf</a>
+                    </li>
+                    <li>
+                        <a href="http://">dsfhsdfhfh</a>
+                    </li>
+                    <li>
+                        <a href="http://">dfhdsfhdh</a>
+                    </li>
+                    <li>
+                        <a href="http://">dfhdhdsfh</a>
+                    </li>
+                    <li>
+                        <a href="http://">sdfhfsdhsdf</a>
+                    </li>
+                    <li>
+                        <a href="http://">sdfhsdfhf</a>
+                    </li>
+                    <li>
+                        <a href="http://">sdfhsdfhhsd</a>
+                    </li>
+                    <li>
+                        <a href="http://">sdfhfsdhsdf</a>
+                    </li>
+                    <li>
+                        <a href="http://">sdfhsdfhf</a>
+                    </li>
+                    <li>
+                        <a href="http://">sdfhsdfhhsd</a>
+                    </li>
+
+                </ul>
+
+                <ul>
+                    <h2>SITES</h2>
+                    <li>
+                        <a href="http://">dfsdsfdshf</a>
+                    </li>
+                    <li>
+                        <a href="http://">dsfhsdfhfh</a>
+                    </li>
+                    <li>
+                        <a href="http://">dfhdsfhdh</a>
+                    </li>
+                    <li>
+                        <a href="http://">dfhdhdsfh</a>
+                    </li>
+
+                </ul>
+            </nav>
         </div>
 
+        <div class="container-due">
+
+            <div class="button">SIGN-UP NOW!</div>
+
+        </div>
+        
     </div>
 
 </footer>
