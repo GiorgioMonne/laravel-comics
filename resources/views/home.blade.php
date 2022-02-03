d784f3ae3389d407109ab349a85bb595992c34f9
Giorgio <1999giorgio.monne@mail.com>
2022-02-03 00:11:40 -1000

    inizio layout

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.base')
+
+
+@section('pageContent')
+    <h1>Home page</h1>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,51 @@
+<header class="header">
+   <div class="container">
+    <div class="logo">
+        <img src="{{asset('img/dc-logo.png')}}" alt="logo dc">
+    </div>
+    <nav>
+        <ul>
+            <li>
+                <a href="/">CHARACRES</a>
+            </li>
+
+            <li>
+                <a href="/">COMICS</a>
+            </li>
+
+            <li>
+                <a href="/">MOVIES</a>
+            </li>
+
+            <li>
+                <a href="/">TV</a>
+            </li>
+
+            <li>
+                <a href="/">GAMES</a>
+            </li>
+
+            <li>
+                <a href="/">COLLECTIBLES</a>
+            </li>
+
+            <li>
+                <a href="/">VIDEOS</a>
+            </li>
+            <li>
+                <a href="/">FANS</a>
+            </li>
+            <li>
+                <a href="/">NEWS</a>
+            </li>
+            <li>
+                <a href="/">SHOP</a>
+            </li>
+        </ul>
+    </nav>
+   </div>
+
+   <div class="jumbo">
+
+   </div>
+</header>
